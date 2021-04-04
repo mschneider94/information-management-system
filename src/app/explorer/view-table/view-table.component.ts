5aef1c4a3bc4b5713cc22f52d17d0d2af281723f
Manuel Schneider <mail@mschneider.cloud>
2021-04-05 00:54:27 +0200

    resolve schemaParents

--- src/app/explorer/view-table/view-table.component.ts
+++ src/app/explorer/view-table/view-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BackendService } from 'src/app/backend.service';
 import { InformationSchema } from 'src/app/information-schema.interface';
 
 @Component({
@@ -9,9 +10,18 @@ import { InformationSchema } from 'src/app/information-schema.interface';
 export class ViewTableComponent implements OnInit {
   @Input() schema: InformationSchema;
 
-  constructor() { }
+  constructor(
+    private backendService: BackendService
+  ) { }
 
   ngOnInit(): void {
+    this.resolveSchema();
     }
 
+  public schemaParents: InformationSchema[] = [];
+  private resolveSchema(): void {
+    for (let parent of this.schema.parents) {
+      this.backendService.getSchema(parent, 'byId').subscribe(schemas => schemas.forEach(schema => this.schemaParents.push(schema)));
+    }
+  }
 }
