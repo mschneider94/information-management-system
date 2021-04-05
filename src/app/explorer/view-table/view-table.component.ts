a7d259bef3f4310ad712c623de22e346b64a8ffb
Manuel Schneider <mail@mschneider.cloud>
2021-04-05 19:55:39 +0200

    optimized InformationSchema Interface (also added a MetaData Interface) and optimized PullData Method from ViewTableComponent

--- src/app/explorer/view-table/view-table.component.ts
+++ src/app/explorer/view-table/view-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BackendService } from 'src/app/backend.service';
 import { InformationData } from 'src/app/information-data.interface';
-import { InformationSchema } from 'src/app/information-schema.interface';
+import { InformationSchema, MetaData } from 'src/app/information-schema.interface';
 
 @Component({
   selector: 'app-view-table',
@@ -22,6 +22,7 @@ export class ViewTableComponent implements OnInit {
 
   public schemaParents: InformationSchema[] = [];
   private resolveSchema(): void {
+    // Get InformationObject for every parent (ObjectID) of schema and store those in this.schema.parents
     for (let parent of this.schema.parents) {
       this.backendService.getSchema(parent, 'byId').subscribe(schemas => schemas.forEach(schema => this.schemaParents.push(schema)));
     }
@@ -29,14 +30,19 @@ export class ViewTableComponent implements OnInit {
 
   public informationData: InformationData[] = [];
   public pullData(): void {
+    // Request all InformationData of current InfromationSchema, subscribe Observable
     this.backendService.getData(this.schema._id, 'bySchema').subscribe(informationDataArray => {
-      informationDataArray.forEach(informationData => {
-        this.informationData.push(informationData);
-      });
-      // Fix parents, ?make recursive?
+      // Push InformationData-Object into this.informationData-Array
+      informationDataArray.forEach(informationData => this.informationData.push(informationData));
+      // Resolv parents of this.informationData only, no necessity for recursion here right now...
+      // ...recursive resolution of parents could lead to inifinte loop, as InformationData could be cross-linked!
+      // Iterate through every element of this.informationData[]
       for (let x = 0; x < this.informationData.length; x++) {
+        // Iterate thorugh every parents[] of current InformationObject
         for (let y = 0; y < this.informationData[x].parents.length; y++) {
-          this.backendService.getData(this.informationData[x].parents[y], 'byId').subscribe(res => this.informationData[x].parents[y] = res);
+          // Request InformationObject for current parent of current InformationObject, subscribe Observable
+          // Instruct Subscription to overwrite current parent:string with requested InformationData (resolve linked documents)
+          this.backendService.getData(this.informationData[x].parents[y], 'byId').subscribe(res => this.informationData[x].parents[y] = res[0]);
         }
       }
     });
--- src/app/information-schema.interface.ts
+++ src/app/information-schema.interface.ts
@@ -1,10 +1,15 @@
+export interface MetaData {
+  name: string;
+  type: string;
+}
+
 export interface InformationSchema {
   _id?: string;
   schemaVersion: string;
   category: string;
   view: string;
-  content: any;
+  content: string;
   displayName: string;
   parents: string[];
-  metaData: any[];
+  metaData: MetaData[];
 }
