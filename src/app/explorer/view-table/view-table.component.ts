fc941623e40044de87883d7a0060885320004277
Manuel Schneider <mail@mschneider.cloud>
2021-04-05 11:59:37 +0200

    extended ViewTableComponent to pullData

--- src/app/explorer/view-table/view-table.component.ts
+++ src/app/explorer/view-table/view-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BackendService } from 'src/app/backend.service';
+import { InformationData } from 'src/app/information-data.interface';
 import { InformationSchema } from 'src/app/information-schema.interface';
 
 @Component({
@@ -16,6 +17,7 @@ export class ViewTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.resolveSchema();
+    this.pullData();
     }
 
   public schemaParents: InformationSchema[] = [];
@@ -23,5 +25,20 @@ export class ViewTableComponent implements OnInit {
     for (let parent of this.schema.parents) {
       this.backendService.getSchema(parent, 'byId').subscribe(schemas => schemas.forEach(schema => this.schemaParents.push(schema)));
     }
+  }
+
+  public informationData: InformationData[] = [];
+  public pullData(): void {
+    this.backendService.getData(this.schema._id, 'bySchema').subscribe(informationDataArray => {
+      informationDataArray.forEach(informationData => {
+        this.informationData.push(informationData);
+      });
+      // Fix parents, ?make recursive?
+      for (let x = 0; x < this.informationData.length; x++) {
+        for (let y = 0; y < this.informationData[x].parents.length; y++) {
+          this.backendService.getData(this.informationData[x].parents[y], 'byId').subscribe(res => this.informationData[x].parents[y] = res);
+        }
+      }
+    });
   }
 }
